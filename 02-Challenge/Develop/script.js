7b5928333e2878b13b92cbb76db9f8ebdf922713
Kxyiv <anthonybivin6@gmail.com>
2022-05-04 14:12:28 -0600

    Added Develop Folder

---
+++ 02-Challenge/Develop/script.js
@@ -0,0 +1,23 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+const arr = [1,2,3,4,5]
+const str =["a", "b", "c", "d", "e", "f"]
+const avail = []
+
+console.log( arr[math.floor(Math.random() * arr.length)]);
+function addItem()
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
